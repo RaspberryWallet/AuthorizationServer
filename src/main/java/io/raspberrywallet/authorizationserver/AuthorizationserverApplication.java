43f1c274ac53779a03e0a2c8d2b030ca9518fe83
PatrykMilewski <patryk.milewski@gmail.com>
2018-10-09 21:59:57 +0200

    Initial commit. First version, basic implementation but working

---
+++ src/main/java/io/raspberrywallet/authorizationserver/AuthorizationserverApplication.java
@@ -0,0 +1,12 @@
+package io.raspberrywallet.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationserverApplication.class, args);
+    }
+}
